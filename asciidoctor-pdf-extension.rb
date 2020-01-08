0338f726db19b8de659a3181370bf70a442c09a1
Andrea Esposito <esposito_andrea99@hotmail.com>
2020-01-08 09:02:03 +0100

    Modifiche minori all'estensione

--- asciidoctor-pdf-extension.rb
+++ asciidoctor-pdf-extension.rb
@@ -1,12 +1,12 @@
-require "asciidoctor-pdf" unless defined? ::Asciidoctor::Pdf
+require 'asciidoctor-pdf' unless defined? ::Asciidoctor::Pdf
 
-module AsciidoctorPdfExtensions
+class AsciidoctorPdfExtension < (Asciidoctor::Converter.for 'pdf')
   # Override the built-in layout_toc to move colophon before front of table of contents
   # NOTE we assume that the colophon fits on a single page
   def layout_toc(doc, num_levels = 2, toc_page_number = 2, start_y = nil, num_front_matter_pages = 0)
     go_to_page toc_page_number unless (page_number == toc_page_number) || scratch?
     if scratch?
-      colophon = doc.find_by(context: :section) { |sect| sect.sectname == "colophon" }
+      colophon = doc.find_by(context: :section) { |sect| sect.sectname == 'colophon' }
       if (colophon = colophon.first)
         doc.instance_variable_set :@colophon, colophon
         colophon.parent.blocks.delete colophon
@@ -28,18 +28,18 @@ module AsciidoctorPdfExtensions
   # force select chapters to start on the recto (odd-numbered, right-hand) page
   def start_new_chapter(chapter)
     start_new_page unless at_page_top?
-    if @ppbook && verso_page? && !(chapter.option? "nonfacing")
+    if @ppbook && verso_page? && !(chapter.option? 'nonfacing')
       update_colors # prevents Ghostscript from reporting a warning when running content is written to blank page
       start_new_page
     end
   end
 
   def layout_chapter_title(node, title, opts = {})
-    if (sect_id = node.id) == "dedication" || sect_id == "acknowledgements"
+    if (sect_id = node.id) == 'dedication' || sect_id == 'acknowledgements'
       layout_heading_custom title, align: :center
-    elsif sect_id == "colophon"
+    elsif sect_id == 'colophon'
       #puts 'Processing ' + node.sectname + '...'
-      if node.document.attr? "media", "prepress"
+      if node.document.attr? 'media', 'prepress'
         move_down 325
       else
         move_down 460
@@ -53,27 +53,25 @@ module AsciidoctorPdfExtensions
   def layout_heading_custom(string, opts = {})
     move_down 100
     typeset_text string, calc_line_metrics((opts.delete :line_height) || @theme.heading_line_height), {
-      inline_format: true,
+        inline_format: true,
     }.merge(opts)
     move_up 5
     i = 0
-    underline = ""
+    underline = ''
     while i < string.length
-      if string == "Dedication"
-        underline += "/////"
-      else
-        underline += "//////"
-      end
+      underline += if string == 'Dedication'
+                     '/////'
+                   else
+                     '//////'
+                   end
       i += 1
     end
-    if string == "Dedication"
-      underline += "////"
+    if string == 'Dedication'
+      underline += '////'
     end
     typeset_text underline, calc_line_metrics((opts.delete :line_height) || @theme.heading_line_height), {
-      inline_format: true, color: "B0B0B0", size: 8, style: :italic,
+        inline_format: true, color: 'B0B0B0', size: 8, style: :italic,
     }.merge(opts)
     move_down 20
   end
 end
-
-Asciidoctor::Pdf::Converter.prepend AsciidoctorPdfExtensions
